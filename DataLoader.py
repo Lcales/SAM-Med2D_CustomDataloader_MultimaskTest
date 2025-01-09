d2df7e446c5dfe15285f1884c4fbe519a78427eb
Lcales <115400656+Lcales@users.noreply.github.com>
2025-01-09 16:06:03 +0100

    Update DataLoader.py
    Prova modifica output nome maschere

--- DataLoader.py
+++ DataLoader.py
@@ -179,7 +179,7 @@ class TrainingDataset(Dataset):
         image_input["boxes"] = boxes
         image_input["point_coords"] = point_coords
         image_input["point_labels"] = point_labels
-        image_input["label_name"] = masks_list
+        image_input["label_name"] = self.label_paths
 
         image_name = self.image_paths[index].split('/')[-1]
         if self.requires_name:
